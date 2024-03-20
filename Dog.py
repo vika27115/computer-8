9afed37a36cc591ff3b2041c9ea647186df8f1e8
vika27115 <158267792+vika27115@users.noreply.github.com>
2024-03-20 10:00:44 +0000

    add to main.py

---
+++ Dog.py
@@ -0,0 +1,14 @@
+class DogSpike:
+  name = 'Spike' #class atrribute
+  def __init__(self):
+    self.x = False 
+
+#   def walk(self):
+#     print('Dog walked')
+#     self.is_walked = True 
+
+my_dog = DogSpike
+
+print(my_dog.name)
+print(my_dog.x)
+# my_dog.walked()
---
+++ new.py
@@ -0,0 +1,6 @@
+class DogSpike:
+  def __init__(self):
+    self.x=1
+
+d=DogSpike()
+print(d.x)

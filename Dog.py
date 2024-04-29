480ce3de82536671590bc19cb417a2e5f1e10b64
vika27115 <158267792+vika27115@users.noreply.github.com>
2024-04-29 16:23:44 +0000

    add to main.py

--- Dog.py
+++ Dog.py
@@ -1,11 +1,23 @@
 class DogSpike:
-  name = 'Spike' #class atrribute
+  name = 'Rex' #class atrribute
   def __init__(self):
-    self.is_walked = False 
+    self.is_sit = False 
+    self.is.stay = False
+    self.is.come = False
 
-  def walk(self):
-    print('Dog walked')
-    self.is_walked = True 
+  def sit.print(self):
+  self.is_sit = True 
+
+   def is_stay.print(self):
+    print('Dog stay')
+    self.is_stay = True 
+
+   def come.print(self):
+    print('Dog come')
+    self.is_come = True 
+    
+  def stay.print(self):
+
 
 my_dog = DogSpike ()
 
